0cd3afcd7b91776bbcec49982573e02c551eaae6
wakfuld23 <63659885+wakfuld23@users.noreply.github.com>
2021-07-16 20:49:23 +0200

    fixed cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose")
 const {MONGO_URI} = require("./config")
+const cors = require("cors")
 const Posts = require("./models/posts")
 
 
